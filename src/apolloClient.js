fa0b076c45a2cd3bb1bcda9558954b01ef2aee08
Rafael Audy Glanzner <rafael.audy@gmail.com>
2019-04-27 13:00:19 +0545

    Adds apollo client

---
+++ src/apolloClient.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000"
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
+const getApolloClient = childComponent => (
+  <ApolloProvider client={client}>{childComponent}</ApolloProvider>
+);
+
+export { getApolloClient };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import { getApolloClient } from "./apolloClient";
 import App from "./components/app/App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(getApolloClient(<App />), document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
