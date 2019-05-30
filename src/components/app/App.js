29f6eaf7cc526e042300556f2b05a8d8861067f0
Rafael Audy Glanzner <rafael.audy@gmail.com>
2019-05-30 11:33:34 +0545

    Adds middleware to attach JWT token

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+
+import Header from "../layout/Header";
+import LinkList from "../link/LinkList";
+import CreateLink from "../link/CreateLink";
+import Login from "../auth/Login";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="center w85">
+        <Header />
+        <div className="ph3 pv1 background-gray">
+          <Switch>
+            <Route exact path="/" component={LinkList} />
+            <Route exact path="/create" component={CreateLink} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </div>
       </div>
     );
   }
---
+++ src/components/link/LinkList.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+
+import Link from "./Link";
+
+class LinkList extends Component {
+  render() {
+    const FEED_QUERY = gql`
+      {
+        feed {
+          links {
+            id
+            url
+            description
+          }
+        }
+      }
+    `;
+
+    return (
+      <div>
+        <Query query={FEED_QUERY}>
+          {({ loading, error, data }) => {
+            if (loading) return <div>Fetching</div>;
+            if (error) return <div>Error</div>;
+
+            const linksToRender = data.feed.links;
+
+            return (
+              <div>
+                {linksToRender.map(link => (
+                  <Link key={link.id} link={link} />
+                ))}
+              </div>
+            );
+          }}
+        </Query>
+      </div>
+    );
+  }
+}
+
+export default LinkList;
